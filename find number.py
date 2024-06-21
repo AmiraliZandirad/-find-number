193a4abbd8815b4ab1c88f4d250273c253118226
AmirAli Zandi Rad <138608178+AmiraliZandi@users.noreply.github.com>
2024-06-22 00:38:22 +0330

    Add files via upload

---
+++ find number.py
@@ -0,0 +1,13 @@
+
+def find_num(mylist):
+    myDic={}
+    for item in mylist:
+        try:
+            myDic[item]+=1
+        except KeyError:
+            myDic[item]=1
+
+    return myDic
+
+nums = find_num([0,2,3,4,3,2,4,])
+print (nums)
